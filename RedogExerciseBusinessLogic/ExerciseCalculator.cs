3ab5a44dc1e0114e578c09c025cb0d445788809f
Michael Meienberg <public@meienberg.cc>
2021-12-13 09:10:08 +0100

    funktionalität schreiben in zellen

---
+++ RedogExerciseBusinessLogic/ExerciseCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedogExerciseBusinessLogic
+{
+    public class ExerciseCalculator
+    {
+
+
+        public ExerciseCalculator()
+        {
+
+        }
+
+        public List<string> Execute()
+        {
+            var dogs = new List<string> { "phinti", "jule", "odin", "mayla" };
+            return dogs;
+        }
+    }
+}
--- RedogExerciseCalculator/Redog.cs
+++ RedogExerciseCalculator/Redog.cs
@@ -6,6 +6,10 @@ using System.Text;
 using System.Windows.Forms;
 using RedogExerciseBusinessLogic;
 
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+
 
 namespace RedogExerciseCalculator
 {
@@ -18,13 +22,31 @@ namespace RedogExerciseCalculator
 
         private void Initialize_Click(object sender, RibbonControlEventArgs e)
         {
-            
-           
+            MessageBox.Show(Class1.getTestresult());
+
         }
+
+        //private void DisplayActiveSheetName()
+        //{
+        //    Excel.Worksheet worksheet1 = (Excel.Worksheet)this.ActiveSheet;
+        //    MessageBox.Show("The name of the active sheet is: " +
+        //        worksheet1.Name);
+        //}
 
         private void Calculate_Click(object sender, RibbonControlEventArgs e)
         {
-            MessageBox.Show(Class1.getTestresult());
+            ExerciseCalculator ec = new ExerciseCalculator();
+           
+            foreach (string dog in ec.Execute())
+            {
+                Excel.Window window = e.Control.Context;              
+                Excel.Worksheet activeWorksheet = ((Excel.Worksheet)window.Application.ActiveSheet);
+                Excel.Range firstRow = activeWorksheet.get_Range("A1");
+                firstRow.EntireRow.Insert(Excel.XlInsertShiftDirection.xlShiftDown);
+                Excel.Range newFirstRow = activeWorksheet.get_Range("A1");
+                newFirstRow.Value2 = dog;
+
+            }
         }
     }
 }
