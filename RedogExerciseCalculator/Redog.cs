22f094daabe46a7e05f9cccd6fc047945a2ebac7
Michael Meienberg <public@meienberg.cc>
2021-12-13 07:55:33 +0100

    Add project files.

---
+++ RedogExerciseCalculator/Redog.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace RedogExerciseCalculator
+{
+    public partial class Redog
+    {
+        private void Redog_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+           MessageBox.Show("hallo");
+        }
+    }
+}
